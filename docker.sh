9d27889a8f923e397575a484db36a0ee58afc71b
root <root@northwyck>
2018-05-29 15:59:04 -0400

    Added initial run and sshfs caching fix.

--- docker.sh
+++ docker.sh
@@ -20,16 +20,21 @@ V_CRON="0 4 * * *"; if [ -n "${CRON}" ]; then V_CRON="${CRON}"; fi
 if [ -n "${S1_SSH_KEY}" ] && [ -n "${S1_SSH_PORT}" ] && [ -n "${S1_SSH_USER}" ] && [ -n "${S1_SSH_HOST}" ] && [ -n "${S1_SSH_PATH}" ]; then
 	echoDN "Mounting sshfs for server 1... "
 	mkdir -p /mnt/S1
-	eval sshfs -o allow_other,StrictHostKeyChecking=no,IdentityFile="${S1_SSH_KEY}" -p ${S1_SSH_PORT} ${S1_SSH_USER}@${S1_SSH_HOST}:"${S1_SSH_PATH}" /mnt/S1
+	eval sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile="${S1_SSH_KEY}" -p ${S1_SSH_PORT} ${S1_SSH_USER}@${S1_SSH_HOST}:"${S1_SSH_PATH}" /mnt/S1
 	V_S1_DB_PATH="/mnt/S1"
 	echo "Done"
 fi
 if [ -n "${S2_SSH_KEY}" ] && [ -n "${S2_SSH_PORT}" ] && [ -n "${S2_SSH_USER}" ] && [ -n "${S2_SSH_HOST}" ] && [ -n "${S2_SSH_PATH}" ]; then
 	echoDN "Mounting sshfs for server 2... "
 	mkdir -p /mnt/S2
-	eval sshfs -o allow_other,StrictHostKeyChecking=no,IdentityFile="${S2_SSH_KEY}" -p ${S2_SSH_PORT} ${S2_SSH_USER}@${S2_SSH_HOST}:"${S2_SSH_PATH}" /mnt/S2
+	eval sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile="${S2_SSH_KEY}" -p ${S2_SSH_PORT} ${S2_SSH_USER}@${S2_SSH_HOST}:"${S2_SSH_PATH}" /mnt/S2
 	V_S2_DB_PATH="/mnt/S2"
 	echo "Done"
+fi
+
+if [ "${INITIALRUN}" == "true" ]; then
+	#echo -e "Running command: /plex-db-sync --dry-run \x22${V_DRYRUN}\x22 --backup \x22${V_BACKUP}\x22 --debug \x22${V_DEBUG}\x22 --tmp-folder \x22${V_TMPFOLDER}\x22 --plex-db-1 \x22${V_S1_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-1 \x22${S1_START}\x22 --plex-stop-1 \x22${S1_STOP}\x22 --plex-db-2 \x22${V_S2_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-2 \x22${S2_START}\x22 --plex-stop-2 \x22${S2_STOP}\x22 --ignore-accounts \x22${IGNOREACCOUNTS}\x22"
+	/plex-db-sync --dry-run "${V_DRYRUN}" --backup "${V_BACKUP}" --debug "${V_DEBUG}" --tmp-folder "${V_TMPFOLDER}" --plex-db-1 "${V_S1_DB_PATH}/com.plexapp.plugins.library.db" --plex-start-1 "${S1_START}" --plex-stop-1 "${S1_STOP}" --plex-db-2 "${V_S2_DB_PATH}/com.plexapp.plugins.library.db" --plex-start-2 "${S2_START}" --plex-stop-2 "${S2_STOP}" --ignore-accounts "${IGNOREACCOUNTS}"
 fi
 
 # Set up cron
@@ -39,9 +44,3 @@ chmod 0644 /crontab.txt
 touch /var/log/cron.log
 crontab /crontab.txt
 crond -f -l 8
-
-
-# Loop to run sync
-#while [ 1 -eq 1 ]; do
-	#eval sleep ${V_INTERVAL}
-#done;
